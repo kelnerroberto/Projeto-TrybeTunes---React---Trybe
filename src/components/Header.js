f31b7b7bad630f81a47025ff7047bc27c17ad4f8
Kelner Roberto <kelner250@hotmail.com>
2021-09-14 20:38:11 -0300

    requisito 3 - renderizaçao do header com o loading

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import Loading from '../pages/Loading';
+import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: '',
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getUserName();
+  }
+
+  async getUserName() {
+    this.setState({
+      loading: true,
+    });
+    const userObject = await getUser();
+    const { name } = userObject;
+    this.setState({
+      username: name,
+      loading: false,
+    });
+  }
+
   render() {
+    const { username, loading } = this.state;
     return (
       <header data-testid="header-component">
-        <h2>oi</h2>
+        <h2 data-testid="header-user-name">
+          {
+            loading ? <Loading /> : username
+          }
+        </h2>
       </header>
     );
   }
