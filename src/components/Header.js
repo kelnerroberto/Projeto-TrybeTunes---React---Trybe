7d734b31f189146f60326ea41ab28fdd7df9859b
Kelner Roberto <kelner250@hotmail.com>
2021-09-14 17:36:47 -0300

    3/4 do requisito 2 - criar input e button para login - pendente: redirecionamento

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header data-testid="page-not-found">
+        oi
+      </header>
+    );
+  }
+}
+
+export default Header;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      disabled: true,
+      name: '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange({ target }) {
+    this.setState({
+      name: target.value,
+    }, () => { this.switchButtonEnability(); });
+  }
+
+  switchButtonEnability() {
+    const { name } = this.state;
+    const MIN_CHARACTERS_USERNAME = 3;
+    const changeEnability = name.length < MIN_CHARACTERS_USERNAME;
+    this.setState({
+      disabled: changeEnability,
+    });
+  }
+
+  loadingAndCreateUser() {
+    const { name } = this.state;
+    createUser({ name });
+  }
+
   render() {
+    const { disabled } = this.state;
     return (
       <div data-testid="page-login">
-        oi
+        <input
+          type="text"
+          data-testid="login-name-input"
+          onChange={ this.handleInputChange }
+        />
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+          id="login-btn"
+          disabled={ disabled }
+          onClick={ () => this.loadingAndCreateUser() }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
