871a43de54f8bff605495810de6878af235e433d
Kelner Roberto <kelner250@hotmail.com>
2021-09-14 21:08:18 -0300

    requisito 5 - faz a page de search com um input e um button

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange({ target }) {
+    this.setState({
+      name: target.value,
+    });
+  }
+
   render() {
+    const { name } = this.state;
+    const MIN_CHARACTERS_USERNAME = 2;
     return (
       <div data-testid="page-search">
-        oi
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          onChange={ this.handleInputChange }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ name.length < MIN_CHARACTERS_USERNAME }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
